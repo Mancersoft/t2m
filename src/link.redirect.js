c2b221cf111908c5ea5b7b5520c4ec377719f4fb
Stanislav Kyian <mancersoft@gmail.com>
2024-11-04 12:29:44 +0000

    Hotfix

--- src/link.redirect.js
+++ src/link.redirect.js
@@ -1,6 +1,6 @@
 if (window.location.search) {
     const link = new URLSearchParams(window.location.search).get('link');
     if (link) {
-        window.open(link, "magnetframe");
+        window.location.replace(link);
     }
 }
